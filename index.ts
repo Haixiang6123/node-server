921eebcbc059f5027aa2cd2563462c23db402f8f
Haixiang <haixiang6123@gmail.com>
2020-03-09 15:37:20 -0700

    Finish target 1

--- index.ts
+++ index.ts
@@ -1,10 +1,35 @@
 import * as http from 'http'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const server = http.createServer()
+const publicDir = path.resolve(__dirname, 'public')
 
 server.on('request', (request, response) => {
-  console.log('request')
-  response.end('hi')
+  const {method, url, headers} = request
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+  }
 })
 
 server.listen(8888)
