37152745a9683207b7ede4117b066706692b7249
Haixiang <haixiang6123@gmail.com>
2020-03-09 15:42:21 -0700

    Finish target 2

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import * as http from 'http'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as url from 'url'
 
 const server = http.createServer()
 const publicDir = path.resolve(__dirname, 'public')
 
 server.on('request', (request, response) => {
-  const {method, url, headers} = request
-  switch (url) {
+  const {method, url: urlPath, headers} = request
+  // 使用 url.parse 解析
+  const {pathname, search} = url.parse(urlPath)
+  switch (pathname) {
     case '/index.html':
       response.setHeader('Content-Type', 'text/html; charset=utf-8')
       fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
@@ -29,6 +32,9 @@ server.on('request', (request, response) => {
         response.end(data.toString())
       })
       break
+    default:
+      response.statusCode = 404
+      response.end()
   }
 })
 
